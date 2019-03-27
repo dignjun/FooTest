3cba704f43dee7a0bbc4580617677b8285ca1b24
DINGJUN <dingjun@chinatvpay.com>
2019-03-27 17:33:04 +0800

    leetcode-3.最大不重复字符子串例程

---
+++ DataStructure/src/main/java/com/example/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,56 @@
+package com.example.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 3.无重复字符的最长子串
+ *
+ * @author DINGJUN
+ * @date 2019.03.27
+ */
+public class LengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) // 字符串的第一个索引
+            for (int j = i + 1; j <= n; j++) // 字符串的第二个索引
+                if (unique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public boolean unique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch))
+                return false;
+            set.add(ch);
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        LengthOfLongestSubstring ls = new LengthOfLongestSubstring();
+        int res = ls.lengthOfLongestSubstring("asddasdfsasdf");
+        System.out.println("无重复字符最长子串:" + res);
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
