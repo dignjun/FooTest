c48d2da483b4d766571d3b2071d8c6b1d74f9ce1
DINGJUN <dingjun@chinatvpay.com>
2019-04-16 15:21:04 +0800

    datastructure-shell and merge sort

--- DataStructure/src/main/java/com/example/sort/SortAlgorithm.java
+++ DataStructure/src/main/java/com/example/sort/SortAlgorithm.java
@@ -11,6 +11,10 @@ public class SortAlgorithm {
         Integer[] arr = {1, 5, 3, 6, 1, 8, 2, 9};
         insertionSort(arr);
         print(arr);
+        System.out.println("----");
+        shellSort(arr);
+        print(arr);
+
     }
 
     /**
@@ -28,6 +32,79 @@ public class SortAlgorithm {
             t[j] = tmp;
         }
     }
+
+    /**
+     * 希尔排序
+     *
+     * @param t
+     * @param <T>
+     */
+    public static <T extends Comparable<? super T>> void shellSort(T[] t){
+        int j;
+        for (int gap = t.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < t.length; i ++) {
+                T tmp = t[i];
+                for (j = i; j >= gap && tmp.compareTo(t[j - gap]) < 0; j-= gap) {
+                    t[j] = t[j - gap];
+                }
+                t[j] = tmp;
+            }
+        }
+    }
+
+
+
+    private static int leftChild(int i) {
+        return 2 * i + 1;
+    }
+    private static <T extends Comparable<? super T>> void percDown(T[] a, int i, int n) {
+        int child=0;
+        T tmp;
+        for (tmp = a[i]; leftChild(i) < n; i = child) {
+            child = leftChild(i);
+            if(child != n -1 && a[child].compareTo(a[child + 1]) < 0)
+                child ++;
+            if (tmp.compareTo(a[child]) < 0)
+                a[i] = a[child];
+            else
+                break;
+        }
+        a[i] = tmp;
+    }
+    public static <T extends Comparable<? super T>> void heapSort(T[] t) {
+        for (int i = t.length/2 -1; i >= 0; i--) // buildHeap
+            percDown(t, i, t.length);
+        for (int i = t.length - 1; i > 0; i --) {
+            // swapReferences(t, 0, i) // deleteMax
+            percDown(t, 0, i);
+        }
+    }
+
+
+    public static <T extends Comparable<? super T>> void merge(T[] a, T[] tmpArray, int leftPos, int rightPos, int rightEnd) {
+        int leftEnd = rightPos - 1;
+        int tmpPos = leftPos;
+        int numElements = rightEnd - leftPos + 1;
+        // Main loop
+        while(leftPos <= leftEnd && rightPos <= rightEnd)
+            if (a[leftPos].compareTo(a[rightPos]) <= 0)
+                tmpArray[tmpPos++] = a[leftPos++];
+            else
+                tmpArray[tmpPos++] = a[rightPos++];
+        while (leftPos <= leftEnd) // copy rest of first half
+            tmpArray[tmpPos++] = a[leftPos++];
+        while (rightPos <= rightEnd) // copy rest of right half
+            tmpArray[tmpPos++] = a[rightPos++];
+
+        // copy tmpArray back
+        for (int i = 0; i < numElements; i++, rightEnd--)
+            a[rightEnd] = tmpArray[rightEnd];
+    }
+
+
+
+
+
 
     // 打印
     public static <T> void print(T[] t) {
