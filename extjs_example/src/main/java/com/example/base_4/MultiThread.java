247535ef8a35dd6581d2bde839d649f7c802d6c5
DINGJUN <dingjun941035104@163.com>
2019-09-15 16:38:22 +0800

    java天生就是多线程的, main方法是由一个叫main的线程执行的.线程优先级并不一定保证程序的执行结果

--- extjs_example/src/main/java/com/example/base_4/MultiThread.java
+++ extjs_example/src/main/java/com/example/base_4/MultiThread.java
@@ -1,7 +1,29 @@
 package com.example.base_4;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
 /**
+ * 1.java程序天生就是多线程程序,因为执行main方法的是一个名称为main的线程,
+ * 下面一下启动一个java程序有多少个线程在运行.
+ * <p>
  * Created by DINGJUN on 2019.09.08.
  */
 public class MultiThread {
+    public static void main(String[] args) {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "] " + threadInfo.getThreadName() + " " + threadInfo.getThreadState());
+        }
+        /**
+         [6] Monitor Ctrl-Break RUNNABLE
+         [5] Attach Listener RUNNABLE
+         [4] Signal Dispatcher RUNNABLE
+         [3] Finalizer WAITING
+         [2] Reference Handler WAITING
+         [1] main RUNNABLE  // 用户线程入口, java程序启动之后这个进程之中会有多个线程在同时运行
+         */
+    }
 }
--- extjs_example/src/main/java/com/example/base_4/Priority.java
+++ extjs_example/src/main/java/com/example/base_4/Priority.java
@@ -1,7 +1,71 @@
 package com.example.base_4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
+ * 线程的优先级
+ * <p>
  * Created by DINGJUN on 2019.09.08.
  */
 public class Priority {
+    private static volatile boolean notStart = true;
+    private static volatile boolean notEnd = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int priority = i < 5 ? Thread.MIN_PRIORITY : Thread.MAX_PRIORITY;
+            Job job = new Job(priority);
+            jobs.add(job);
+            Thread thread = new Thread(job, "Thread: " + i);
+            thread.setPriority(priority);
+            thread.start();
+        }
+        notStart = false;
+        TimeUnit.SECONDS.sleep(10);
+        notEnd = false;
+        for (Job job : jobs) {
+            System.out.println("job priority: " + job.priority + ", count: " + job.jobCount);
+        }
+        /**
+         job priority: 1, count: 60113979
+         job priority: 1, count: 95557243
+         job priority: 1, count: 95106080
+         job priority: 1, count: 94990165
+         job priority: 1, count: 95381534
+         job priority: 10, count: 70048991
+         job priority: 10, count: 95562625
+         job priority: 10, count: 95310741
+         job priority: 10, count: 95946571
+         job priority: 10, count: 47199574
+         */
+    }
+
+    /**
+     * 构建所有的线程, notStart之前都在等待, 构造完所有线程之后, 将notStart变为false,所有线程同时竞争开始运行
+     * 按理说, 优先级高的线程应该早运行的,也就是计数应该更高的,但是看到运行的结果, java优先级并不能保证程序结果的正确性.
+     *
+     */
+    static class Job implements Runnable { // 工作线程, 实现了Runnable接口, 优先级和任务数量的成员
+
+        private int priority;
+        private long jobCount;
+
+        public Job(int priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public void run() {
+            while (notStart) {
+                Thread.yield(); // 让线程进入到就绪状态.
+            }
+            while (notEnd) {
+                Thread.yield();
+                jobCount++;
+            }
+        }
+    }
 }
